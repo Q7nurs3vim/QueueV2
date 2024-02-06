4c309ce6db5ddc13d08617e9498e4d595f805a80
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2024-02-06 11:24:14 +0300

    Add files via upload

---
+++ QueueV2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace QueueV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GİŞE 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "GİŞE2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(295, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GİŞE3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(25, 106);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(10, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = ",";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(159, 106);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(10, 13);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = ".";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(295, 106);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(10, 13);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = ",";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnEkle.Location = new System.Drawing.Point(25, 220);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(377, 49);
+            this.btnEkle.TabIndex = 7;
+            this.btnEkle.Text = "MÜŞTERİ EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(428, 317);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ QueueV2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueV2
+{
+    public partial class Form1 : Form
+    {
+        Queue<String> musteri = new Queue<String>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void musteriCagir(Label lbl)
+        {
+            if (musteri.Count > 0)
+            {
+                lbl.Text = musteri.Dequeue();
+            }
+            else
+            {
+                MessageBox.Show("Müsteriler Bitti !!!!!!!!!");
+            }
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            musteri.Enqueue(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lbl1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lbl2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lbl3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            musteri.Enqueue("Nursevim");
+            musteri.Enqueue("Hilal");
+            musteri.Enqueue("Yezda");
+            musteri.Enqueue("Ravza");
+        }
+    }
+}
